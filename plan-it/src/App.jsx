02d1b79b7b232f527f9c9197c78e20dd03d32946
subodhsalgaonkar <145000831+subodhsalgaonkar@users.noreply.github.com>
2024-07-11 16:42:07 +0530

    big-calendar added

--- plan-it/src/App.jsx
+++ plan-it/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import MyCalendar from './components/Calendar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div></div>
+      <div style={{}}>
+        <MyCalendar/>
+      </div>
     </>
   )
 }
---
+++ plan-it/src/components/Calendar.jsx
@@ -0,0 +1,19 @@
+import { Calendar, dayjsLocalizer } from 'react-big-calendar'
+import dayjs from 'dayjs'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+const localizer = dayjsLocalizer(dayjs)
+
+const MyCalendar = ({ events }) => (
+  <div>
+    <Calendar
+      localizer={localizer}
+      events={events}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+    />
+  </div>
+)
+
+export default MyCalendar;
