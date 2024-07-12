cb58e50d8b4ac3328565bb98d57ace5c3ae75611
elitehitman <shintreaditya05@gmail.com>
2024-07-12 19:22:48 +0530

    backend started

--- plan-it/src/components/Calendar.jsx
+++ plan-it/src/components/Calendar.jsx
@@ -7,14 +7,14 @@ const localizer = dayjsLocalizer(dayjs);
 
 const MyCalendar = ({ events }) => (
   // <div className="">
-    <Calendar
-      localizer={localizer}
-      events={events}
-      startAccessor="start"
-      endAccessor="end"
-      style={{height:'91.2vh'}}
-      className="fixed bottom-0 right-0 w-10/12"
-    />
+  <Calendar
+    localizer={localizer}
+    events={events}
+    startAccessor="start"
+    endAccessor="end"
+    style={{ height: "91.2vh" }}
+    className="fixed bottom-0 right-0 w-10/12"
+  />
   // </div>
 );
 
