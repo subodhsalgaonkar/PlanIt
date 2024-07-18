58da54c409abfb2cfac64e69df535dc2f036ca7d
subodhsalgaonkar <145000831+subodhsalgaonkar@users.noreply.github.com>
2024-07-18 20:36:26 +0530

    Event card details component added: Events are clickable 2

--- plan-it/src/components/EventDetailsCard.jsx
+++ plan-it/src/components/EventDetailsCard.jsx
@@ -1,12 +1,13 @@
 // EventDetailsCard.js
 import React from 'react';
+// import eventbackground from "../assets/eventbackground.jpg";
 
 const EventDetailsCard = ({ event, onClose }) => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="relative bg-white p-8 rounded-lg shadow-lg w-full max-w-md max-h-60 overflow-y-auto">
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 bg-fixed ">
+      <div className="relative p-8 rounded-lg shadow-lg w-full max-w-md max-h-60 overflow-y-auto bg-blue-300" >
         <button
-          className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+          className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 h-10 w-10 rounded-full hover:bg-blue-400"
           onClick={onClose}
         >
           &times;
